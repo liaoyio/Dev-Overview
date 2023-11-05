1c4aefa6d596998deb8984dc9193ee02f556cb29
liaoyi <2417276459@qq.com>
2023-11-05 15:37:27 +0800

    feat: 添加 RightSidebar 组件 :sparkles:

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSiderbar from '@/components/shared/RightSiderbar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -6,13 +8,12 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSidebar
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           {children}
         </section>
-        RightSiderbar
+        <RightSiderbar />
       </div>
-      Toaster
     </main>
   )
 }
