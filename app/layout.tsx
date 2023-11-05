ebf746c125a44d7ab33f251140396fab760318c2
liaoyi <2417276459@qq.com>
2023-11-05 10:05:20 +0800

    style: 导入项目静态资源,修改layout布局 :art:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,46 @@
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import { Metadata } from 'next'
+import { ThemeProvider } from '@/context/ThemeProvider'
+import '@/styles/globals.css'
 
-export const metadata = {
-  title: 'Next.js 13 with Clerk'
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const space_Grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-space-grotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description:
+    'DevFlow is a platform for developers to share their projects and collaborate with others.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${space_Grotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'promary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500'
+            }
+          }}
+        >
+          {children}
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
