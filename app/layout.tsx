74e7765cb967d21590b8c6cfc8b0e102778361d3
liaoyi <2417276459@qq.com>
2023-11-05 12:06:38 +0800

    fix: 修复 layout.tsx :bug:

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             }
           }}
         >
-          {children}
           <ThemeProvider>{children}</ThemeProvider>
         </ClerkProvider>
       </body>
