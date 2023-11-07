805b201f8f2bc3f97282a52522071d5e21236341
liaoyi <2417276459@qq.com>
2023-11-07 12:51:37 +0800

    feat: 添加 clerk auth webhook :sparkles:

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,11 @@
 'use server'
 
 import User from '@/database/user.model'
+import Question from '@/database/question.model'
 import { connectToDatabase } from '../mongoose'
+
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from './shared'
+import { revalidatePath } from 'next/cache'
 
 import { IUser } from '@/database/user.model'
 
@@ -14,6 +18,60 @@ export async function getUserById(params: any): Promise<IUser> {
     const user = await User.findOne({ clerkId: userId })
 
     return user
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase()
+
+    const newUser = await User.create(userData)
+    return newUser
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId, updateData, path } = params
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true
+    })
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase()
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({ clerkId })
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    // get user question ids
+    const userQuestionIds = await Question.find({ author: user._id }).distinct('_id')
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id })
+
+    const deletedUser = await User.findByIdAndDelete(user._id)
+
+    return deletedUser
   } catch (error) {
     console.log(error)
     throw error
