1e56e267a7dfcbf61dd3be2d4887294b29fd008a
liaoyi <2417276459@qq.com>
2023-11-07 15:03:08 +0800

    fix: 将api/webhook添加到公共路由 (修复请求失败问题) :bug:

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
 import { authMiddleware } from '@clerk/nextjs'
 
-// 设置白名单路由
+// add api/webhook to public routes （public routes are not protected by Clerk）
 export default authMiddleware({
-  publicRoutes: ['/']
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    'question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
+  ],
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt']
 })
 
 export const config = {
