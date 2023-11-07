95d3f1fcb097fe2edfbeb8c2cf622c54d130b04a
liaoyi <2417276459@qq.com>
2023-11-07 21:55:34 +0800

    fix: 修复不同域名用户头像显示报错 :bug:

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    mdxRs: true,
+    serverComponentsExternalPackages: ['mongoose']
+  },
   images: {
-    domains: ['avatars.githubusercontent.com'],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.githubusercontent.co'
-      }
+      { protocol: 'https', hostname: '*' },
+      { protocol: 'http', hostname: '*' }
     ]
   }
 }
