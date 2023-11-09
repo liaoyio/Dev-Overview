80c0141b9145d331dde88bbd73cb2a8dd880a585
liaoyi <2417276459@qq.com>
2023-11-09 10:55:13 +0800

    feat: 实现首页 LocalSearchbar 交互 :sparkles:

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -13,11 +13,23 @@ import type {
   QuestionVoteParams
 } from './shared'
 
+import { FilterQuery } from 'mongoose'
+
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase()
 
-    const questions = await Question.find({})
+    const { searchQuery } = params
+    const query: FilterQuery<typeof Question> = {}
+
+    if (searchQuery) {
+      query.$or = [
+        { title: { $regex: new RegExp(searchQuery, 'i') } },
+        { content: { $regex: new RegExp(searchQuery, 'i') } }
+      ]
+    }
+
+    const questions = await Question.find(query)
       .populate({ path: 'tags', model: Tag })
       .populate({ path: 'author', model: User })
       .sort({ createdAt: -1 })
