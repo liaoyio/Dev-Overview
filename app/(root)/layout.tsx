9a60043544d7e48cee61a68f43887235379d7215
liaoyi <2417276459@qq.com>
2023-11-23 12:26:57 +0800

    style: 取消页面 border 边框 :art:

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Navbar />
       <div className="flex">
         <LeftSidebar />
-        <section className="flex min-h-screen flex-1 flex-col border-t border-yellow-600 px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           {children}
         </section>
         <RightSiderbar />
